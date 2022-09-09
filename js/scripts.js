8ba66d29dda012d1665e00212ee1d01f05b0735a
Johnny <johnny.noun@isae.edu.lb>
2022-09-09 23:58:57 +0300

    git commit -m "new skills"

--- js/scripts.js
+++ js/scripts.js
@@ -74,3 +74,22 @@ $(document).ready(function() {
   $(".back").css("height", highest);
   $(absoluteSide).css("position", "absolute");
 });
+
+//image slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
